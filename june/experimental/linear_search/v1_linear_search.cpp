76d8af486d38e1c590974ba7e1068daec3932db0
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-13 22:54:33 +0630

    linear search experimental v1 working condition

--- june/experimental/linear_search/v1_linear_search.cpp
+++ june/experimental/linear_search/v1_linear_search.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 using namespace std;
 
+bool linearSearch(int array[], int array_size, int search_value,
+                  int *position) {
+  bool return_value = false;
+  for (int i = 0; i < array_size; i++) {
+    if (array[i] == search_value) {
+      return_value = true;
+      *position = i;
+      break;
+    }
+  }
+  return return_value;
+}
+
 int main() {
-  int array[5] = {
+  int array[] = {
       5, 56, 65, 5665, 6556,
   };
+  int search_value, position;
+  int array_size = sizeof(array) / sizeof(array[0]);
+
+  cout << "\tLINEAR SEARCH" << endl;
+  cout << "\n- Enter search value: ";
+  cin >> search_value;
+
+  bool found = linearSearch(array, array_size, search_value, &position);
+
+  if (found) {
+    cout << "\n\tFOUND at position: " << position << endl;
+  } else {
+    cout << "\n\tNOT FOUND" << endl;
+  }
 
   return 0;
 }
