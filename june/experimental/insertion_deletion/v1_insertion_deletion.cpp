d0fe739d4bde931cd0c56fe91318c6efbb81d97c
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-13 20:25:03 +0630

    insertion deletion experimental v1 working condition

--- june/experimental/insertion_deletion/v1_insertion_deletion.cpp
+++ june/experimental/insertion_deletion/v1_insertion_deletion.cpp
@@ -2,26 +2,34 @@
 using namespace std;
 
 int main() {
-  int array_size;
-  string array[array_size];
+  int array_size, index;
+  string array[array_size], value;
   bool run_loop = true;
 
-  cout << "Number of elements in the array: ";
+  cout << "\tInsertion Deletion in an Array" << endl;
+  cout << "\t------------------------------\n" << endl;
+  cout << "- Number of elements in the array: ";
   cin >> array_size;
 
-  while (run_loop) {
-    string mode;
-    cout << "Do you want to insert \"i\", delete (\"d\") or quit (\"q\")? ";
-    cin >> mode;
+  cout << "- Index of the element: ";
+  cin >> index;
+  while (index < 0 || index >= array_size) {
+    cout << "\n! Index out of bounds!\n- Index valid from 0...up to "
+         << array_size - 1 << ": ";
+    cin >> index;
+  }
 
-    if (mode == "q") {
-      run_loop = false;
-    } else if (mode == "i") {
-      string value;
-      int index;
+  cout << "- Name of the element: ";
+  cin >> value;
+  cout << "\n\tARRAY ELEMENTS" << endl;
 
-      cout << "Enter index: ";
+  array[index] = value;
+
+  for (int i = 0; i < array_size; i++) {
+    if (i != index) {
+      array[i] = "---";
     }
+    cout << " " << i << ".\t" << array[i] << endl;
   }
 
   return 0;
