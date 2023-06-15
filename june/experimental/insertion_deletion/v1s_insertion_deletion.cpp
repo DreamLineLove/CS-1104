bfa4b82e59150d6667ab1bbaad1b51be96a5791e
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-15 22:21:55 +0630

    insertion deletion experimental v1 wrote a shorter version

---
+++ june/experimental/insertion_deletion/v1s_insertion_deletion.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int checkIndexOutOfBounds(string mode, int array_size) {
+  int index;
+  if (mode == "insertion") {
+    cout << "index you would like to insert at: ";
+    cin >> index;
+  } else if (mode == "deletion") {
+    cout << "index you would like to delete from: ";
+    cin >> index;
+  }
+  while (index < 0 || index >= array_size) {
+    cout << "\nindex must be between 0 and " << array_size - 1 << "!" << endl;
+    cout << "index you would like to insert at: ";
+    cin >> index;
+  }
+
+  return index;
+}
+
+void insertion(int index, string array[], int array_size) {
+  string input;
+  cout << "\nname of the element: ";
+  cin >> input;
+  array[index] = input;
+
+  cout << "\nArray elements" << endl;
+  for (int i = 0; i < array_size; i++) {
+    cout << array[i] << endl;
+  }
+}
+
+void deletion(int index, string array[], int array_size) {
+  array[index] = "\"\"";
+  cout << "\nArray elements" << endl;
+  for (int i = 0; i < array_size; i++) {
+    cout << array[i] << endl;
+  }
+}
+
+int main() {
+  int array_size;
+  cout << "size of the array: ";
+  cin >> array_size;
+
+  string array[array_size];
+  for (int i = 0; i < array_size; i++) {
+    array[i] = "\"\"";
+  }
+
+  int index;
+  index = checkIndexOutOfBounds("insertion", array_size);
+  insertion(index, array, array_size);
+  cout << endl;
+  index = checkIndexOutOfBounds("deletion", array_size);
+  deletion(index, array, array_size);
+
+  return 0;
+}
