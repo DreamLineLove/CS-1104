14f0326ddf11ad9d1fa7b8ee3c37f187f5ae461f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-17 16:08:35 +0630

    june experiments tuto1 no5 working condition

--- june/experimental/tutorials/tuto1_june16/tuto1_no5_june16.cpp
+++ june/experimental/tutorials/tuto1_june16/tuto1_no5_june16.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 using namespace std;
 
@@ -17,12 +18,26 @@ int main() {
 
   ofstream studentsFile;
   studentsFile.open("studentsData.txt");
+  studentsFile << "No." << setw(22) << "Names" << setw(8) << "Marks" << endl;
+  studentsFile << "---" << setw(22) << "--------------------" << setw(8)
+               << "-----" << endl;
   int i;
   for (i = 0; i < 5; i++) {
-    studentsFile << " " << names[i] << "\t" << marks[i] << endl;
+    studentsFile << i + 1 << ". " << setw(22) << names[i] << setw(5) << marks[i]
+                 << endl;
   }
-  if (i == 5)
-    studentsFile << "\tTotal:\t" << total << endl;
+  if (i == 5) {
+    studentsFile << "\n" << setw(25) << "Total:" << setw(6) << total << endl;
+  }
+  studentsFile.close();
+
+  ifstream readFile;
+  readFile.open("studentsData.txt");
+  string line;
+  while (getline(readFile, line)) {
+    cout << line << endl;
+  }
+  readFile.close();
 
   return 0;
 }
