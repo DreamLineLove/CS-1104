ba18d16ce98245b655c91b218af1422e3dc41aa7
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-18 20:31:28 +0630

    june experimental tuto1 no6 explore kinda understood

--- june/experimental/tutorials/tuto1_june16/explore_no6.cpp
+++ june/experimental/tutorials/tuto1_june16/explore_no6.cpp
@@ -2,21 +2,28 @@
 using namespace std;
 
 int main() {
-  string array[] = {"one", "two", "three", "four", "five", "six", "seven"};
-  int length = sizeof(array) / sizeof(array[0]);
+  string people[] = {"Min Thu", "Cherry", "Thura", "Thar Htoo", ""};
+  int length = sizeof(people) / sizeof(people[0]);
 
-  cout << "Original array:\n";
+  cout << "Original array..." << endl;
   for (int i = 0; i < length; i++) {
-    cout << array[i] << endl;
+    cout << "\t\"" << people[i] << "\"" << endl;
   }
 
-  for (int i = length - 2; i >= 1; i--) {
-    array[i + 1] = array[i];
+  for (int i = 3; i > 1; i--) {
+    people[i + 1] = people[i];
+  }
+  people[2] = "Thiha";
+  cout << "\nAfter inserting \"Thiha\"..." << endl;
+  for (int i = 0; i < length; i++) {
+    cout << "\t\"" << people[i] << "\"" << endl;
   }
 
-  cout << endl;
+  people[3] = people[4];
+  people[4] = "";
+  cout << "\nAfter deleting \"Thura\"..." << endl;
   for (int i = 0; i < length; i++) {
-    cout << array[i] << endl;
+    cout << "\t\"" << people[i] << "\"" << endl;
   }
 
   return 0;
