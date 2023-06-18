c7189bd22d54935700a9dd5888a8710d3ba5603b
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-18 20:19:13 +0630

    june experimental tuto1 no6 explore starting to understand

--- june/experimental/tutorials/tuto1_june16/explore_no6.cpp
+++ june/experimental/tutorials/tuto1_june16/explore_no6.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
-  string array[5] = {"one", "two", "three", "four", "five"};
+  string array[] = {"one", "two", "three", "four", "five", "six", "seven"};
+  int length = sizeof(array) / sizeof(array[0]);
+
+  cout << "Original array:\n";
+  for (int i = 0; i < length; i++) {
+    cout << array[i] << endl;
+  }
+
+  for (int i = length - 2; i >= 1; i--) {
+    array[i + 1] = array[i];
+  }
+
+  cout << endl;
+  for (int i = 0; i < length; i++) {
+    cout << array[i] << endl;
+  }
 
   return 0;
 }
