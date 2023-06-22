33668115648cd43c491cc2db2e0f15b338159cb4
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-23 04:54:15 +0630

    explored strcpy a little

---
+++ june/experimental/strings/v1_strings_module4_experiments.cpp
@@ -0,0 +1,26 @@
+#include <cctype>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+void reverseCases(char *string, int size);
+
+int main() {
+  // strcpy
+  char blumentopf[80];
+  char blumen[] = "Vier Blumen stecken in dem Blumentopf.";
+  strcpy(blumentopf, blumen);
+  cout << blumentopf << endl;
+  cout << endl;
+}
+
+void reverseCases(char *string, int size) {
+  for (int i = 0; i < size; i++) {
+    if (islower(*string)) {
+      *string = toupper(*string);
+    } else if (isupper(*string)) {
+      *string = tolower(*string);
+    }
+    string++;
+  }
+}
