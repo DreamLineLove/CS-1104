b6e1ac046461899cf64389ff368494eed07afe7a
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-24 01:14:05 +0630

    june submitted module5 example3

---
+++ june/submitted/module5_example3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void f();
+
+int main() {
+	cout << "Before call\n";
+	f();
+	cout << "After call\n";
+	
+	return 0;
+}
+
+void f() {
+	cout << "Inside f()\n";
+	return;
+	cout << "This won't display!";
+}
