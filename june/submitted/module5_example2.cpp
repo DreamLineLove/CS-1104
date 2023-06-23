3b657e7e183771a8a35ed4fdc49088817f9700a8
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-24 01:13:51 +0630

    june submitted module5 example2

---
+++ june/submitted/module5_example2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void box(int length, int width, int height);
+
+int main() {
+	box(7, 20, 4);
+	box(50, 3, 2);
+	box(8, 6, 9);	
+	
+	return 0;
+}
+
+void box(int length, int width, int height) {
+	cout << "volume of the box is " << length * width * height << "\n";
+}
