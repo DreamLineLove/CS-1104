9459646b1c8d4a1c449a9d9c5684e7d895e9b471
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-24 01:21:14 +0630

    june submitted module4 exercise1

---
+++ june/submitted/Module4/Exercises/module4_exercise1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int multi[24];
+	int i;
+	
+	for (i = 2; i < 25; i++) multi[i] = 9 * i;
+	
+	for (i = 2; i < 25; i++) cout << "9 x " << i << " = " << multi[i] << endl;
+	
+	return 0;
+}
