8649817b0673b7eead72c7b04708492187496f18
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-24 01:20:28 +0630

    june submitted module4 example1

---
+++ june/submitted/Module4/Examples/module4_example1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int sample[10];
+	int t;
+	
+	for (t = 0; t < 10; ++t) sample[t] = t;
+	
+	for (t = 0; t < 10; ++t) cout << "This is sample[" << t << "]: " << sample[t] << "\n";
+	
+	return 0;
+}
