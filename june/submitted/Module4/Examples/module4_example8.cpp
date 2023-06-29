34c0fd11bdebe2468790eeb16f356b76924313eb
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-29 20:08:32 +0630

    june submitted Module4 example8 not finished

---
+++ june/submitted/Module4/Examples/module4_example8.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i;
+  char str[80];
+  char phone_numbers[10][80] = {
+      "Tom",    "555-3322", "Mary",   "555-8976",
+      "Rachel", "555-1400", "Sherry", "555-8873",
+  };
+
+  cout << "Enter name: ";
+  cin >> str;
+
+  for (i = 0; i < 10; i += 2) {
+    if (!strcmp(str, phone_numbers[i])) {
+      cout << "Number is " << phone_numbers[i + 1] << endl;
+    }
+  }
+
+  if (i == 10)
+    cout << "Not found." << endl;
+
+  return 0;
+}
