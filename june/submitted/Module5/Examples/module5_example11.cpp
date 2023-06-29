b30b261121823aba4397bcb7f9db5717fe17e4f7
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-30 00:35:12 +0630

    june submitted Module5 example11 addition

---
+++ june/submitted/Module5/Examples/module5_example11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Enter a number: ";
+  int a;
+  cin >> a;
+
+  cout << "Enter a second number: ";
+  int b;
+  cin >> b;
+
+  cout << "Product of two numbers: " << a * b << endl;
+
+  return 0;
+}
