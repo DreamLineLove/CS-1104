bcd443aec7b20d29023174d70bfd059564812799
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-30 00:33:39 +0630

    june submitted Module5 example10 addition

---
+++ june/submitted/Module5/Examples/module5_example10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 19;
+
+  if (x == 19) {
+    int y = 20;
+
+    cout << "x + y is " << x + y << endl;
+  }
+
+  // Error! y not known here (out of its scope)
+  // y = 100;
+
+  return 0;
+}
