a32c5f6e5364d812a461392abc740117c9a5e36b
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-01 19:42:02 +0630

    june submitted Module5 example23 addition

---
+++ june/submitted/Module5/Examples/module5_example23.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int factr(int n);
+int fact(int n);
+
+int main() {
+  cout << "4 factorial is " << factr(4);
+  cout << endl;
+
+  cout << "4 factorial is " << fact(4);
+  cout << endl;
+
+  return 0;
+}
+
+// Recursive version
+int factr(int n) {
+  int answer;
+  if (n == 1)
+    return (1);
+  answer = factr(n - 1) * n;
+  return (answer);
+}
+
+// Iterative version
+int fact(int n) {
+  int t, answer;
+
+  answer = 1;
+  for (t = 1; t <= n; t++)
+    answer = answer * (t);
+  return (answer);
+}
