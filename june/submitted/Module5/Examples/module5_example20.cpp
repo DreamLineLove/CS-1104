864273b0a9eeb4920f83b5ec443d5dd055b7463f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-01 19:29:34 +0630

    june submitted Module5 example20 experiment

--- june/submitted/Module5/Examples/module5_example20.cpp
+++ june/submitted/Module5/Examples/module5_example20.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
@@ -5,6 +6,9 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     cout << argv[i] << endl;
   }
+
+  char isee[] = "9.99";
+  cout << atof(isee);
 
   return 0;
 }
