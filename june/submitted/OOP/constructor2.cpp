2312de75b9c57537da06f239aa1fa22254c85533
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-14 21:56:13 +0630

    OOP constructor2

---
+++ june/submitted/OOP/constructor2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+	public:
+		string brand;
+		string model;
+		int year;
+		Car(string x, string y, int z) {
+			brand = x;
+			model = y;
+			year = z;
+		}
+};
+
+int main() {
+	Car BMW("BMW", "X5", 1999);
+	Car Ford("Ford", "Mustang", 1969);
+	
+	cout << BMW.brand << " " << BMW.model << " " << BMW.year << endl;
+	cout << Ford.brand << " " << Ford.model << " " << Ford.year << endl;
+	 
+	return 0;
+}
