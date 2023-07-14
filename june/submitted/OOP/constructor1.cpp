0df9b46349144242db5f4ff26264d880df35b8c6
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-14 21:55:50 +0630

    OOP constructor1

---
+++ june/submitted/OOP/constructor1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+	public: 
+		int birth_year;
+		string name;
+		Person() {
+			cout << "Hello World!" << endl;
+		}
+};
+
+int main() {
+	Person Znz();
+	
+	return 0;
+}
