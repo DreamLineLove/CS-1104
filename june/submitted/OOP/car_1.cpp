84b3b3e49e79ed31120b5dbfbaca41d6d7248538
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-14 21:53:48 +0630

    OOP car1

---
+++ june/submitted/OOP/car_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+	public: 
+		int year;
+		string manufacturer;
+};
+
+int main() {
+	Car toyota;
+	toyota.year = 2014;
+	toyota.manufacturer = "Toyota";
+	
+	cout << toyota.year << endl;
+	cout << toyota.manufacturer << endl;
+	
+	return 0;
+}
