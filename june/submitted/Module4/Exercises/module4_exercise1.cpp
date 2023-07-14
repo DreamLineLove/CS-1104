1ed725cc6244b606c5e6d6dbb276f20c793a5e1e
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-14 21:52:02 +0630

    OOP first commit

--- june/submitted/Module4/Exercises/module4_exercise1.cpp
+++ june/submitted/Module4/Exercises/module4_exercise1.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main() {
-	int multi[24];
-	int i;
-	
-	for (i = 2; i < 25; i++) multi[i] = 9 * i;
-	
-	for (i = 2; i < 25; i++) cout << "9 x " << i << " = " << multi[i] << endl;
-	
-	return 0;
+  int multi[24];
+  int i;
+
+  for (i = 2; i < 25; i++)
+    multi[i] = 9 * i;
+
+  for (i = 2; i < 25; i++)
+    cout << "9 x " << i << " = " << multi[i] << endl;
+
+  return 0;
 }
