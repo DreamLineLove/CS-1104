04a87b07fd1644fc9f80ae5d25c408c79d992d83
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-15 20:37:01 +0630

    OOP student array rewrite not-tested-20

--- june/submitted/OOP/student_array.cpp
+++ june/submitted/OOP/student_array.cpp
@@ -11,7 +11,7 @@ public:
   Student() { cout << "\tstudent object constructed..." << endl; }
   void gather_information() {
     cout << "\nWhat is the name of the student? ";
-    cin >> name;
+    getline(cin, name);
     for (int i = 0; i < 6; i++) {
       cout << "\n-\tSubject " << i + 1 << " name: ";
       cin.ignore();
