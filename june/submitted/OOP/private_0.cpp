766841cbb4b22ff6d9d1ca4aab3f93ce9eece8e1
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-16 23:50:10 +0630

    OOP private0 constructor furthered

--- june/submitted/OOP/private_0.cpp
+++ june/submitted/OOP/private_0.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 class Custom {
 public:
-  Custom() { cout << "Custom object constructed"; }
+  Custom() {
+    cout << "Custom object constructed";
+    constructed = true;
+  }
+  bool constructed = false;
 
 private:
   string strState;
   int intState;
 };
 
-int main() { return 0; }
+int main() {
+  Custom mein;
+  if (mein.constructed == 1)
+    cout << "constructed: true";
+
+  return 0;
+}
