3d26d73c6107dba760b85002de81e65b8a3bd2e6
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-17 18:25:50 +0630

    tutorial 1 re-adding 8

---
+++ june/submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no6_june16.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string people[] = {"Min Thu", "Cherry", "Thura", "Thar Htoo", ""};
+  int length = sizeof(people) / sizeof(people[0]);
+
+  cout << "Original array..." << endl;
+  for (int i = 0; i < length; i++) {
+    cout << "\t\"" << people[i] << "\"" << endl;
+  }
+
+  for (int i = 3; i > 1; i--) {
+    people[i + 1] = people[i];
+  }
+  people[2] = "Thiha";
+  cout << "\nAfter inserting \"Thiha\"..." << endl;
+  for (int i = 0; i < length; i++) {
+    cout << "\t\"" << people[i] << "\"" << endl;
+  }
+
+  people[3] = people[4];
+  people[4] = "";
+  cout << "\nAfter deleting \"Thura\"..." << endl;
+  for (int i = 0; i < length; i++) {
+    cout << "\t\"" << people[i] << "\"" << endl;
+  }
+
+  return 0;
+}
