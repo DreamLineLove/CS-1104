ba9b39beb071e26e64fadb72931244b3f482ac5e
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-18 14:56:23 +0630

    assignment3 no_4b naming refactor

--- submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-  int base[2][2] = {
+  int base_numbers[2][2] = {
       {2, 3},
       {3, 5},
   };
-  int multiplier[2][2] = {
+  int multipliers[2][2] = {
       {1, 2},
       {0, 10},
   };
@@ -14,15 +14,15 @@ int main() {
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 2; j++) {
       cout << "\t";
-      cout << base[0][i] << " * " << multiplier[0][j] << " = "
-           << base[0][i] * multiplier[0][j] << endl;
+      cout << base_numbers[0][i] << " * " << multipliers[0][j] << " = "
+           << base_numbers[0][i] * multipliers[0][j] << endl;
     }
     cout << "\t  ...." << endl;
     if (i == 1) {
       for (int z = 0; z < 2; z++) {
         cout << "\t";
-        cout << base[1][z] << " * " << multiplier[1][1] << " = "
-             << base[1][z] * multiplier[1][1] << endl;
+        cout << base_numbers[1][z] << " * " << multipliers[1][1] << " = "
+             << base_numbers[1][z] * multipliers[1][1] << endl;
         cout << "\t  ......" << endl;
       }
     }
