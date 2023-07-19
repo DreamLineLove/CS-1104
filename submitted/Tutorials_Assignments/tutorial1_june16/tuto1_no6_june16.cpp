922e5c2214b91df92390a694f3d115d0fd540eb2
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-19 11:07:56 +0630

    tutorial1 no6 naming refactor

--- submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no6_june16.cpp
+++ submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no6_june16.cpp
@@ -2,28 +2,28 @@
 using namespace std;
 
 int main() {
-  string people[] = {"Min Thu", "Cherry", "Thura", "Thar Htoo", ""};
-  int length = sizeof(people) / sizeof(people[0]);
+  string names[] = {"Min Thu", "Cherry", "Thura", "Thar Htoo", ""};
+  int len = sizeof(names) / sizeof(names[0]);
 
   cout << "Original array..." << endl;
-  for (int i = 0; i < length; i++) {
-    cout << "\t\"" << people[i] << "\"" << endl;
+  for (int i = 0; i < len; i++) {
+    cout << "\t\"" << names[i] << "\"" << endl;
   }
 
   for (int i = 3; i > 1; i--) {
-    people[i + 1] = people[i];
+    names[i + 1] = names[i];
   }
-  people[2] = "Thiha";
+  names[2] = "Thiha";
   cout << "\nAfter inserting \"Thiha\"..." << endl;
-  for (int i = 0; i < length; i++) {
-    cout << "\t\"" << people[i] << "\"" << endl;
+  for (int i = 0; i < len; i++) {
+    cout << "\t\"" << names[i] << "\"" << endl;
   }
 
-  people[3] = people[4];
-  people[4] = "";
+  names[3] = names[4];
+  names[4] = "";
   cout << "\nAfter deleting \"Thura\"..." << endl;
-  for (int i = 0; i < length; i++) {
-    cout << "\t\"" << people[i] << "\"" << endl;
+  for (int i = 0; i < len; i++) {
+    cout << "\t\"" << names[i] << "\"" << endl;
   }
 
   return 0;
