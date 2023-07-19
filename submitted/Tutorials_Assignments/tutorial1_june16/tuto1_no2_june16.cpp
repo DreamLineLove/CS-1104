b9ffe41c2603654219ad82942972b4f8ca28a508
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-19 10:55:33 +0630

    tutorial1 no2 naming refactor

--- submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no2_june16.cpp
+++ submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no2_june16.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-  int numbers[4] = {2, 3, 4, 5};
-  int array[4][4];
+  int nums[4] = {2, 3, 4, 5};
+  int arr[4][4];
   int i, j;
 
   for (i = 0; i < 4; i++) {
     for (j = 0; j < 4; j++) {
-      array[i][j] = numbers[i] * pow(numbers[i], j);
-      cout << array[i][j] << "\t";
+      arr[i][j] = nums[i] * pow(nums[i], j);
+      cout << arr[i][j] << "\t";
     }
     cout << endl;
   }
