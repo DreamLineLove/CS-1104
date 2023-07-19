cda01b318d53a358325ab6ef86226c7106968cb0
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-19 18:12:46 +0630

    assignment3 no_5a initial

---
+++ submitted/Tutorials_Assignments/assignment3_july17/no_5a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int nums[4][4] = {
+      {2, 3, 4, 5},
+      {4, 9, 16, 25},
+      {8, 27, 64, 125},
+      {16, 81, 144, 625},
+  };
+
+  return 0;
+}
