ee7cb4968e26c80f8db1ebc0909bdef3a7d2e538
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-19 10:47:01 +0630

    tutorial1 no1 naming refactor

--- submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no1_june16.cpp
+++ submitted/Tutorials_Assignments/tutorial1_june16/tuto1_no1_june16.cpp
@@ -3,47 +3,47 @@
 #include <iostream>
 using namespace std;
 
-int countLowercaseLetters(char spell[], int string_length);
+int lowercaseCount(char str[], int string_length);
 
 int main() {
-  char spell[14] = "Avada kedavra";
+  char str[14] = "Avada kedavra";
   int count = 0;
-  cout << "- Original string:\n\t" << spell << endl;
-  count = countLowercaseLetters(spell, 14);
+  cout << "- Original string:\n\t" << str << endl;
+  count = lowercaseCount(str, 14);
   cout << "\tThere are " << count
        << " lowercase letters in the original string." << endl;
 
-  for (int i = 0; i < strlen(spell); i++) {
-    if (spell[i] == 'k') {
-      spell[i] = toupper(spell[i]);
+  for (int i = 0; i < strlen(str); i++) {
+    if (str[i] == 'k') {
+      str[i] = toupper(str[i]);
     }
   }
-  cout << "\n- Start with capital case, other letters lower case:\n\t" << spell
+  cout << "\n- Start with capital case, other letters lower case:\n\t" << str
        << endl;
 
-  count = countLowercaseLetters(spell, 14);
+  count = lowercaseCount(str, 14);
   cout << "\tThere are " << count << " lowercase letters in the new string."
        << endl;
 
-  for (int i = strlen(spell) - 1; i >= 0; i--) {
-    if (isupper(spell[i])) {
-      spell[i] = tolower(spell[i]);
-    } else if (islower(spell[i])) {
-      spell[i] = toupper(spell[i]);
+  for (int i = strlen(str) - 1; i >= 0; i--) {
+    if (isupper(str[i])) {
+      str[i] = tolower(str[i]);
+    } else if (islower(str[i])) {
+      str[i] = toupper(str[i]);
     }
   }
-  cout << "\n- Reversed and inverted string:\n\t" << spell << endl;
-  count = countLowercaseLetters(spell, 14);
+  cout << "\n- Reversed and inverted string:\n\t" << str << endl;
+  count = lowercaseCount(str, 14);
   cout << "\tThere are " << count << " lowercase letters in the new string."
        << endl;
 
   return 0;
 }
 
-int countLowercaseLetters(char spell[], int string_length) {
+int lowercaseCount(char str[], int string_length) {
   int count = 0;
   for (int i = 0; i < string_length; i++) {
-    if (islower(spell[i])) {
+    if (islower(str[i])) {
       count++;
     }
   }
