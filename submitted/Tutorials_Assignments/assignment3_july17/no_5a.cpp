27b9000ad2b20041f9227c175e6dafe248ba05b9
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-19 18:20:23 +0630

    assignment3 no_5a tested

--- submitted/Tutorials_Assignments/assignment3_july17/no_5a.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_5a.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void display(int i) { cout << "\t" << i; }
+
 int main() {
   int nums[4][4] = {
       {2, 3, 4, 5},
@@ -8,6 +10,13 @@ int main() {
       {8, 27, 64, 125},
       {16, 81, 144, 625},
   };
+  int i, j;
 
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      display(nums[i][j]);
+    }
+    cout << endl;
+  }
   return 0;
 }
