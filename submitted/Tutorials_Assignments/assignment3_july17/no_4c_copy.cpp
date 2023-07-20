219d3d7cf36046ba288c160b749677fa996079e3
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-20 21:40:06 +0630

    assignment3 no_4c default version renewed

--- submitted/Tutorials_Assignments/assignment3_july17/no_4c_copy.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_4c_copy.cpp
@@ -1,37 +1,52 @@
 // The default version attempts to implement forward and reverse Selection Sort
 // (depending on minimum or maximum value) in separate functions and returns the
 // resulting value directly.
+// This version of no_4c copies the original array first before perfomring
+// swapping (Selection Sort) operations inside the minNum and maxNum functions.
+// It is otherwise the same as the default version.
 
 #include <iostream>
 using namespace std;
 
 int minNum(int arr[], int size) {
-  int min_num;
+  int newArr[size], min_num;
   for (int i = 0; i < size; i++) {
-    int min = i;
+    newArr[i] = arr[i];
+  }
 
-    for (int j = i; j < size; j++) {
-      if (arr[j] > arr[min]) {
-        swap(arr[j], arr[min]);
+  for (int j = 0; j < size; j++) {
+    int min = j;
+
+    for (int z = j; z < size; z++) {
+      if (newArr[z] > newArr[min]) {
+        int temp = newArr[z];
+        newArr[z] = newArr[min];
+        newArr[min] = temp;
       }
     }
-    min_num = arr[min];
+    min_num = newArr[min];
   }
 
   return min_num;
 }
 
 int maxNum(int arr[], int size) {
-  int max_num;
+  int newArr[size], max_num;
   for (int i = 0; i < size; i++) {
-    int max = i;
+    newArr[i] = arr[i];
+  }
 
-    for (int j = i; j < size; j++) {
-      if (arr[j] < arr[max]) {
-        swap(arr[j], arr[max]);
+  for (int j = 0; j < size; j++) {
+    int max = j;
+
+    for (int z = j; z < size; z++) {
+      if (newArr[z] < newArr[max]) {
+        int temp = newArr[z];
+        newArr[z] = newArr[max];
+        newArr[max] = temp;
       }
     }
-    max_num = arr[max];
+    max_num = newArr[max];
   }
 
   return max_num;
