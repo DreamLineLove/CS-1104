4a17013ac1c019f7753ead90f9462a37cdf32b28
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-21 03:41:00 +0630

    assignment3 made fundamental tweaks

--- submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
@@ -1,13 +1,33 @@
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//   int i, j, arr[6][11];
+//
+//   for (i = 2; i < 6; i++) {
+//     for (j = 1; j < 11; j++) {
+//       arr[i][j] = i * j;
+//       cout << i << " * " << j << " = " << arr[i][j] << endl;
+//     }
+//     cout << endl;
+//   }
+//
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-  int i, j, arr[6][11];
+  int numbers[] = {2, 3, 4, 5};
+  int size[10];
+  int multi[4][10];
 
-  for (i = 2; i < 6; i++) {
-    for (j = 1; j < 11; j++) {
-      arr[i][j] = i * j;
-      cout << i << " * " << j << " = " << arr[i][j] << endl;
+  for (int i = 0; i < 4; i++) {
+
+    for (int j = 0; j < 10; j++) {
+      multi[i][j] = numbers[i] * (j + 1);
+      cout << numbers[i] << " x " << (j + 1) << " = " << multi[i][j] << endl;
     }
     cout << endl;
   }
