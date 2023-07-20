fe80a9b6d5549a75526af1f99799051ca4a2a47d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-21 02:54:45 +0630

    assignment3 no_4b default version rewrite

--- submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
@@ -2,29 +2,15 @@
 using namespace std;
 
 int main() {
-  int base_numbers[2][2] = {
-      {2, 3},
-      {3, 5},
-  };
-  int multipliers[2][2] = {
-      {1, 2},
-      {0, 10},
-  };
+  int arr[4][10];
 
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++) {
-      cout << "\t";
-      cout << base_numbers[0][i] << " * " << multipliers[0][j] << " = "
-           << base_numbers[0][i] * multipliers[0][j] << endl;
+  for (int i = 2; i < 5; i++) {
+    for (int j = 1; j < 11; j++) {
+      arr[i][j] = i * j;
+      cout << "\t" << i << " * " << j << " = " << arr[i][j] << endl;
     }
-    cout << "\t  ...." << endl;
-    if (i == 1) {
-      for (int z = 0; z < 2; z++) {
-        cout << "\t";
-        cout << base_numbers[1][z] << " * " << multipliers[1][1] << " = "
-             << base_numbers[1][z] * multipliers[1][1] << endl;
-        cout << "\t  ......" << endl;
-      }
-    }
+    cout << endl;
   }
+
+  return 0;
 }
---
+++ submitted/Tutorials_Assignments/assignment3_july17/no_4b_literal.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int base_numbers[2][2] = {
+      {2, 3},
+      {3, 5},
+  };
+  int multipliers[2][2] = {
+      {1, 2},
+      {0, 10},
+  };
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      cout << "\t";
+      cout << base_numbers[0][i] << " * " << multipliers[0][j] << " = "
+           << base_numbers[0][i] * multipliers[0][j] << endl;
+    }
+    cout << "\t  ...." << endl;
+    if (i == 1) {
+      for (int z = 0; z < 2; z++) {
+        cout << "\t";
+        cout << base_numbers[1][z] << " * " << multipliers[1][1] << " = "
+             << base_numbers[1][z] * multipliers[1][1] << endl;
+        cout << "\t  ......" << endl;
+      }
+    }
+  }
+}
