b43c91f1b093785bd83cec269df97c36830b57c2
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-21 03:37:29 +0630

    assignment3 no_5a default rewrite

--- submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_4b.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
   int i, j, arr[6][11];
+
   for (i = 2; i < 6; i++) {
     for (j = 1; j < 11; j++) {
       arr[i][j] = i * j;
--- submitted/Tutorials_Assignments/assignment3_july17/no_5a.cpp
+++ submitted/Tutorials_Assignments/assignment3_july17/no_5a.cpp
@@ -1,22 +1,17 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
-void display(int i) { cout << "\t" << i; }
-
 int main() {
-  int nums[4][4] = {
-      {2, 3, 4, 5},
-      {4, 9, 16, 25},
-      {8, 27, 64, 125},
-      {16, 81, 144, 625},
-  };
-  int i, j;
+  int i, j, arr[5][4];
 
-  for (i = 0; i < 4; i++) {
-    for (j = 0; j < 4; j++) {
-      display(nums[i][j]);
+  for (i = 1; i < 5; i++) {
+    for (j = 2; j <= 5; j++) {
+      arr[i][j] = pow(j, i);
+      cout << arr[i][j] << "\t";
     }
     cout << endl;
   }
+
   return 0;
 }
