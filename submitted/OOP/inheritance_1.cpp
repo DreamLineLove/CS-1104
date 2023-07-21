a65b99bcb1423b392f9e48c10ce879a518634234
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-21 20:03:30 +0630

    OOP inheritance_1 re-added from lecture

---
+++ submitted/OOP/inheritance_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+	public:
+		string brand = "Ford";
+		void honk() {
+			cout << "Tuut, tuut! \n";
+		}
+};
+
+class Car: public Vehicle {
+	public:
+		string model = "Mustang";
+};
+
+int main() {
+	Car myCar;
+	myCar.honk();
+	cout << myCar.brand + " " + myCar.model << endl;
+	
+	return 0;
+}
