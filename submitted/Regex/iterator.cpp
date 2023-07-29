f5bffddd9a9f387616bcb2c7e52e645f55848303
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-29 23:30:21 +0630

    iterator.cpp not tested

--- submitted/Regex/iterator.cpp
+++ submitted/Regex/iterator.cpp
@@ -6,8 +6,16 @@ int main() {
   regex pattern("female");
   string str = "female Female female Female";
 
+  sregex_iterator itr(str.begin(), str.end(), pattern);
+  sregex_iterator end;
+
   cout << "using regex_match: " << regex_match(str, pattern) << endl;
   cout << "using regex_search: " << regex_search(str, pattern) << endl;
+
+  int count = 0;
+  while (itr != end) {
+    count++;
+  }
 
   return 0;
 }
