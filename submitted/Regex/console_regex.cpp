13afd6e7d54356b5f4cc4be0f0c37c6d35851333
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-30 18:23:20 +0630

    console_regex enum matching attempted, not tested

--- submitted/Regex/console_regex.cpp
+++ submitted/Regex/console_regex.cpp
@@ -2,18 +2,28 @@
 #include <regex>
 using namespace std;
 
-enum Options { whole, once, multiple };
+enum Options { whole, once, multiple, none };
 
-// Options option() {
-//   regex patternWhole("whole", regex_constants::icase);
-//   regex patternOnce("once", regex_constants::icase);
-//   regex patternMulti("multi.?", regex_constants::icase);
-// }
+Options option(string arg) {
+  regex patternWhole("whole", regex_constants::icase);
+  regex patternOnce("once", regex_constants::icase);
+  regex patternMulti("multi.?", regex_constants::icase);
+
+  if (regex_match(arg, patternWhole)) {
+    return whole;
+  } else if (regex_match(arg, patternOnce)) {
+    return once;
+  } else if (regex_match(arg, patternMulti)) {
+    return multiple;
+  }
+
+  return none;
+}
 
 void printHelp(string name) {
   cout << "#\tThe pattern is\t"
        << "./program_name type" << endl;
-  cout << "\nThree types are availabe: whole, once, and multi." << endl;
+  cout << "\nThree options are availabe: whole, once, and multi." << endl;
   cout << "-\twhole implements regex_match" << endl;
   cout << "-\tonce implements regex_search" << endl;
   cout << "-\tmulti implements sregex_iterator" << endl;
@@ -27,6 +37,12 @@ int main(int argc, char *argv[]) {
   }
   if (argc > 2) {
     cout << "#\tOnly one command line argument accepted!" << endl;
+    printHelp(argv[0]);
+  }
+
+  Options chosen = option(argv[1]);
+  if (chosen == none) {
+    cout << "\n#\tPlease choose an existing option!" << endl;
     printHelp(argv[0]);
   }
 
