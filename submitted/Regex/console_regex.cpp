600608437c636a02687fd3add2253ee0566a742c
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-30 18:10:45 +0630

    regex program using cli started, not tested

---
+++ submitted/Regex/console_regex.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+enum Options { whole, once, multiple };
+
+Options option() {
+  regex patternWhole("whole", regex_constants::icase);
+  regex patternOnce("once", regex_constants::icase);
+  regex patternMulti("multi.?", regex_constants::icase);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    cout << "One command line argument accepted!" << endl;
+    cout << "The pattern is\t"
+         << "./program_name type" << endl;
+    cout << "\nThree types are availabe: whole, once, and multi." << endl;
+    cout << "\twhole implements regex_match" << endl;
+    cout << "\tonce implements regex_search" << endl;
+    cout << "\tmulti implements sregex_iterator" << endl;
+    cout << "\nExample:\t" << argv[0] << " whole" << endl;
+  }
+  if (argc > 2) {
+    cout << "Only one command line argument accepted!" << endl;
+    cout << "The pattern is\t"
+         << "./program_name type" << endl;
+    cout << "\nThree types are availabe: whole, once, and multi." << endl;
+    cout << "\twhole implements regex_match" << endl;
+    cout << "\tonce implements regex_search" << endl;
+    cout << "\tmulti implements sregex_iterator" << endl;
+    cout << "\nExample:\t" << argv[0] << " whole" << endl;
+  }
+
+  return 0;
+}
