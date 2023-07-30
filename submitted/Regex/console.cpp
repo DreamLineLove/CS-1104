08a6d7e9e26396cd5c41d216563c459e8fb67b90
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-30 17:44:32 +0630

    basic command-line-argument gathering logic

---
+++ submitted/Regex/console.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout << argv[0] << endl;
+  cout << argv[1] << endl;
+  cout << argv[2] << endl;
+
+  return 0;
+}
