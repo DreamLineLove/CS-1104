bb6e233a46d0b5d70df31805788d717ea2d1bdf7
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-30 18:14:59 +0630

    unified help printing logic proposed

--- submitted/Regex/console_regex.cpp
+++ submitted/Regex/console_regex.cpp
@@ -4,22 +4,26 @@ using namespace std;
 
 enum Options { whole, once, multiple };
 
-Options option() {
-  regex patternWhole("whole", regex_constants::icase);
-  regex patternOnce("once", regex_constants::icase);
-  regex patternMulti("multi.?", regex_constants::icase);
+// Options option() {
+//   regex patternWhole("whole", regex_constants::icase);
+//   regex patternOnce("once", regex_constants::icase);
+//   regex patternMulti("multi.?", regex_constants::icase);
+// }
+
+void printHelp(string name) {
+  cout << "#\tThe pattern is\t"
+       << "./program_name type" << endl;
+  cout << "\nThree types are availabe: whole, once, and multi." << endl;
+  cout << "-\twhole implements regex_match" << endl;
+  cout << "-\tonce implements regex_search" << endl;
+  cout << "-\tmulti implements sregex_iterator" << endl;
+  cout << "\nExample:\t" << name << " whole" << endl;
 }
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
-    cout << "One command line argument accepted!" << endl;
-    cout << "The pattern is\t"
-         << "./program_name type" << endl;
-    cout << "\nThree types are availabe: whole, once, and multi." << endl;
-    cout << "\twhole implements regex_match" << endl;
-    cout << "\tonce implements regex_search" << endl;
-    cout << "\tmulti implements sregex_iterator" << endl;
-    cout << "\nExample:\t" << argv[0] << " whole" << endl;
+    cout << "#\tOne command line argument required!" << endl;
+    printHelp(argv[0]);
   }
   if (argc > 2) {
     cout << "Only one command line argument accepted!" << endl;
