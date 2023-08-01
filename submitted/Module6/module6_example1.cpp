188400cd9527a3d158db5b41c30a9074fd411c77
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-01 10:40:07 +0630

    Module6 example2, not renamed yet

---
+++ submitted/Module6/module6_example1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y);
+
+int main() {
+  int i, j;
+
+  i = 10;
+  j = 20;
+
+  cout << "Initial values of i and j: ";
+  cout << i << ' ' << j << endl;
+
+  swap(&j, &i);
+
+  cout << "Swapped values of i and j: ";
+  cout << i << ' ' << j << endl;
+
+  return 0;
+}
+
+void swap(int *x, int *y) {
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
