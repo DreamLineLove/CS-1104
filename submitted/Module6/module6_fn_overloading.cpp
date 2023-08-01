379f01e9d32d73050495b743c01163d63093e942
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-01 15:09:13 +0630

    module6 default arguments, tried by myself

--- submitted/Module6/module6_fn_overloading.cpp
+++ submitted/Module6/module6_fn_overloading.cpp
@@ -13,5 +13,8 @@ int main() {
   cout << "Area of the rectangle is: " << area(5.7, 5.0) << endl;
   cout << "Area of the cirlce is: " << area(6) << endl;
 
+  say();
+  say("I am saying something!");
+
   return 0;
 }
