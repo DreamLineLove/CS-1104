4154705b4b1d4e295b63b2e4591d29a63fd0ae99
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-01 15:08:43 +0630

    module6 function overloading tried by myself

---
+++ submitted/Module6/module6_fn_overloading.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+
+double area(double x, double y) { return x * y; }
+
+double area(double r) { return PI * r * r; }
+
+void say(string str = "Lorem Ipsm") { cout << str << endl; }
+
+int main() {
+  cout << "Area of the rectangle is: " << area(5.7, 5.0) << endl;
+  cout << "Area of the cirlce is: " << area(6) << endl;
+
+  return 0;
+}
