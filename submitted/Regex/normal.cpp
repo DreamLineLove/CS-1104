3179dfdee538da94a4a072503a360ef43e31afad
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-04 21:24:54 +0630

    string cli without regex initialized

---
+++ submitted/Regex/normal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string search, fn;
+
+  cout << "Please enter filename...  ";
+  cin >> fn;
+  cout << "Please enter string to search...  ";
+  cin >> search;
+
+  return 0;
+}
