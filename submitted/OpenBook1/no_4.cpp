196ad59987149ef0779df37ba235f29a6f9457d7
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-04 11:59:45 +0630

    Initial logic furthered

--- submitted/OpenBook1/no_4.cpp
+++ submitted/OpenBook1/no_4.cpp
@@ -9,6 +9,21 @@ const int dollarRate = 3500;
 const int yuanRate = 500;
 const int yenRate = 20;
 
-int main() { return 0; }
+int main() {
+  cout << "\tCURRENCY EXCHANGE" << endl;
+  cout << "\n-\tType in 0 to quit" << endl;
+
+  int value;
+  while (value != 0) {
+    cout << "\n- Value: ";
+    cout << "= " << dollar(value) << " USD" << endl;
+    cout << "= " << yuan(value) << " Yuan" << endl;
+    cout << "= " << yen(value) << " Yen" << endl;
+  }
+
+  return 0;
+}
 
 int dollar(int kyat) { return kyat * dollarRate; }
+int yuan(int kyat) { return kyat * yuanRate; }
+int yen(int kyat) { return kyat * yenRate; }
