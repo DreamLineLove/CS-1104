e1b49b093ab0447c8379964622491c9ba245e3a6
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-06 05:21:18 +0630

    program to take screenshots for powerpoint

--- submitted/Regex/simple1.cpp
+++ submitted/Regex/simple1.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main() {
-  string value;
-  cin >> value;
+  string search_word = "preamble";
 
-  string word = "sample";
-  if (value == word) {
-    cout << "value contains sample";
-  } else {
-    cout << "value does not contain sample";
+  string arr[2][3] = {
+      {"sample", "Sample", "example"},
+      {"Sample", "sample", "preamble"},
+  };
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (arr[i][j] == search_word) {
+        cout << search_word << " found at location " << i << ":" << j << endl;
+      }
+    }
   }
 }
