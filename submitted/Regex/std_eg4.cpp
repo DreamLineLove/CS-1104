8d75be93168c688492ccb24b095f0df32847a0e9
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-06 19:07:55 +0630

    example4 (regex_replace) written

---
+++ submitted/Regex/std_eg4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main() {
+  string str = "The Rangoon College is the oldest college in Myanmar's "
+               "education system and the best known college in Myanmar.";
+
+  cout << str << "\n\n";
+
+  regex pattern("Myanmar");
+
+  string new_str = regex_replace(str, pattern, "Burma");
+  cout << new_str << endl;
+
+  return 0;
+}
