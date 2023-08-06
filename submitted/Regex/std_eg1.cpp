381ecbaf95c59db63e3ad27efa702c1c9e9e4c8d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-06 13:29:16 +0630

    minor changes

--- submitted/Regex/std_eg1.cpp
+++ submitted/Regex/std_eg1.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int main() {
   string str1 =
       "The art of programming is not just about knowing. Programming is about"
-      "knowing clearly, completely, specifically and contextually.";
-  cout << str1 << endl;
+      " knowing clearly, completely, specifically and contextually.";
+  cout << str1 << endl << endl;
 
   regex p1("programming");
 
-  cout << "programming" << endl;
-  cout << "using regex_search:\t" << endl;
-  if (regex_search(str1, p1)) {
+  cout << "-\tusing regex_match:\t";
+  if (regex_match(str1, p1)) {
     cout << "yes" << endl;
   } else {
     cout << "no" << endl;
   }
 
-  cout << "using regex_match:\t" << endl;
-  if (regex_match(str1, p1)) {
+  cout << "programming" << endl;
+  cout << "-\tusing regex_search:\t";
+  if (regex_search(str1, p1)) {
     cout << "yes" << endl;
   } else {
     cout << "no" << endl;
