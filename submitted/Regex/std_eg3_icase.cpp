e8dd761aa85403c0bfd102597cd2be94882f65a3
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-06 18:59:54 +0630

    example3 with icase added

---
+++ submitted/Regex/std_eg3_icase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iterator>
+#include <regex>
+using namespace std;
+
+int main() {
+  string str = "The Rangoon College is the oldest college in Myanmar's "
+               "education system and the best known college in Myanmar.";
+
+  cout << str << "\n\n";
+
+  regex pattern("College", regex_constants::icase);
+
+  sregex_iterator begin(str.begin(), str.end(), pattern);
+  sregex_iterator end;
+
+  int count = distance(begin, end);
+  cout << "matched " << count << " time\n";
+
+  return 0;
+}
