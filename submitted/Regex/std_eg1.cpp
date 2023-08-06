467068952ddf022324e42ee532d35363e0c52d7b
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-06 13:27:35 +0630

    minor changes

--- submitted/Regex/std_eg1.cpp
+++ submitted/Regex/std_eg1.cpp
@@ -6,17 +6,19 @@ int main() {
   string str1 =
       "The art of programming is not just about knowing. Programming is about"
       "knowing clearly, completely, specifically and contextually.";
+  cout << str1 << endl;
 
   regex p1("programming");
 
-  cout << "programming:\t" << endl;
+  cout << "programming" << endl;
+  cout << "using regex_search:\t" << endl;
   if (regex_search(str1, p1)) {
     cout << "yes" << endl;
   } else {
     cout << "no" << endl;
   }
 
-  cout << "programming:\t" << endl;
+  cout << "using regex_match:\t" << endl;
   if (regex_match(str1, p1)) {
     cout << "yes" << endl;
   } else {
