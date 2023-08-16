5226c687fdb21b6012d78b7c99a2a6615a5d7eaf
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-16 23:47:14 +0630

    Added Open Book no_12 pow()

---
+++ submitted/OpenBook1/no_12.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+
+  cout << "Square of number using pow()..." << pow(num, 2);
+
+  return 0;
+}
