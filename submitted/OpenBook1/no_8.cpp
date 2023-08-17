174a9b8801ade55929ab1ad431d37b50c0d9e526
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-17 20:34:03 +0630

    Added OpenBook1 no_8 default version

---
+++ submitted/OpenBook1/no_8.cpp
@@ -0,0 +1,27 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  char arr[5][2][80] = {
+      {"Aung Aung", "Yangon"}, {"Aye Aye", "Mon"},    {"Thin Thin", "Mon"},
+      {"Ei Ei", "Yangon"},     {"Zaw Zaw", "Yangon"},
+  };
+
+  int count = 0;
+
+  char address[80];
+  cout << "Enter address to search: ";
+  cin >> address;
+
+  cout << "\n\tFound\n";
+  for (int i = 1; i < 5; i++) {
+    if (!strcmp(arr[i][1], address)) {
+      cout << arr[i][0] << "\t" << arr[i][1] << endl;
+      count++;
+    }
+  }
+  cout << "\n\tNumber of students: " << count << endl;
+
+  return 0;
+}
