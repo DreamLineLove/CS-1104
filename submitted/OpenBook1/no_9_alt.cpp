9d5ca3bea5a3d2cd9aa7b43cbd129b3220a30e73
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-17 20:13:27 +0630

    Added an alternative version to OpenBook1 no_9

---
+++ submitted/OpenBook1/no_9_alt.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, b;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  for (int i = 1; i < n; i++) {
+    int a = pow((n - 1), 2);
+    b = a + 1;
+  }
+
+  cout << "The result is " << b << endl;
+
+  return 0;
+}
