229532e408bc59c2543fb1ed3c049b423e602668
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-17 20:35:20 +0630

    OpenBook1 no_8 minor changes

--- submitted/OpenBook1/no_8.cpp
+++ submitted/OpenBook1/no_8.cpp
@@ -14,14 +14,14 @@ int main() {
   cout << "Enter address to search: ";
   cin >> address;
 
-  cout << "\n\tFound\n";
+  cout << "\n\tMatches\n";
   for (int i = 1; i < 5; i++) {
     if (!strcmp(arr[i][1], address)) {
       cout << arr[i][0] << "\t" << arr[i][1] << endl;
       count++;
     }
   }
-  cout << "\n\tNumber of students: " << count << endl;
+  cout << "\n\tCount: " << count << endl;
 
   return 0;
 }
