baae754314f33275bb12bceb915ff7bfe2a09074
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-27 17:44:56 +0630

    ControlStructures sequence1 added

---
+++ submitted/ControlStructure/sequence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 5, b = 3;
+
+  int sum = a + b;
+  int diff = a - b;
+  int prod = a * b;
+
+  cout << sum << ' ' << diff << ' ' << prod << endl;
+}
