0fc595da5e8b4c50c00e0cc9f2b7f7c42f41919d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-27 18:30:40 +0630

    ControlStructures dowhile added

---
+++ submitted/ControlStructure/dowhile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 0;
+
+  do {
+    cout << i << " is less than 5"
+         << "\n";
+    i++;
+  } while (i < 5);
+
+  return 0;
+}
