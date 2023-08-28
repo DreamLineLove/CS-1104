706c92b22b36493251706fcc453f95e1e6821ea4
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-28 22:09:36 +0630

    Regex endl change binaries

--- submitted/Regex/smatch1.cpp
+++ submitted/Regex/smatch1.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <regex>
 #include <string>
+using namespace std;
 
 int main() {
-  std::string text = "Hello, my name is John Doe and I am 30 years old.";
-  std::regex pattern(R"(\b\w+\b)");
+  string text = "Hello, my name is John Doe and I am 30 years old.";
+  regex pattern(R"(\b\w+\b)");
 
-  std::smatch matches;
-  if (std::regex_search(text, matches, pattern)) {
+  smatch matches;
+  if (regex_search(text, matches, pattern)) {
     // Accessing the entire matched substring
-    std::cout << "Matched: " << matches.str() << std::"\n";
+    cout << "Matched: " << matches.str() << "\n";
 
     // Iterating over capturing groups (individual words)
     for (const auto &match : matches) {
       // Getting the matched word (capturing group)
-      std::cout << "Captured: " << match << std::"\n";
+      cout << "Captured: " << match << "\n";
     }
   }
 
