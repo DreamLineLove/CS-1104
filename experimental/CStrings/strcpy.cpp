f15d169c8bd08be65231a328a353999edb95d0e2
DreamLineLove <zwenyanzaw@protonmail.com>
2023-08-29 19:23:05 +0630

    added strcpy

--- experimental/CStrings/strcpy.cpp
+++ experimental/CStrings/strcpy.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+void newline() { cout << "\n"; }
+
 int main() {
   char sample[] = "This is a character array string in C++!";
   char sto1[50];
@@ -12,6 +14,9 @@ int main() {
   cout << sto2 << "\n";
 
   cout << strcat(sto1, sto2) << "\n";
+
+  char str1[] = "string1";
+  char str2[] = "string2";
 
   return 0;
 }
