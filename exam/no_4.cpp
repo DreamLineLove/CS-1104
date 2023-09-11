67de8eba60c93f0dc397039c318adcff4540660d
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-11 22:42:01 +0630

    no_4 correct version

--- exam/no_4.cpp
+++ exam/no_4.cpp
@@ -7,48 +7,38 @@ void selection_sort(int arr[], int n);
 int main() {
   int arr[] = {520, 491, 503, 476, 454, 439};
 
-  for (int i = 0; i < 5; i++) {
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 0; i < n; i++) {
     cout << arr[i] << ' ';
   }
   cout << '\n';
 
-  selection_sort(arr, 5);
+  selection_sort(arr, n);
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < n; i++) {
     cout << arr[i] << ' ';
   }
   cout << '\n';
+
   return 0;
 }
-
-// void selection_sort(int arr[], int n) {
-//   int i, j;
-//
-//   for (i = 0; i < n - 1; i++) {
-//     int minIndex = i;
-//     for (j = i + 1; j < n; j++) {
-//       if (arr[j] < arr[minIndex]) {
-//         minIndex = j;
-//       }
-//       if (minIndex != 1) {
-//         swap(arr[minIndex], arr[i]);
-//       }
-//     }
-//   }
-// }
 
 void selection_sort(int arr[], int n) {
   int i, j;
 
   for (i = 0; i < n - 1; i++) {
     int minIndex = i;
+
     for (j = i + 1; j < n; j++) {
-      if (arr[j] << arr[minIndex]) {
+      if (arr[j] < arr[minIndex])
         minIndex = j;
-      }
-      if (minIndex != 1) {
-        swap(arr[minIndex], arr[i]);
-      }
+    }
+
+    if (minIndex != i) {
+      int temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
     }
   }
 }
