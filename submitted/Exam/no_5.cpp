535d0a1fe928f0e520a204265b23e64dd9cece2b
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-11 18:58:23 +0630

    Static vs nonstatic

--- submitted/Exam/no_5.cpp
+++ submitted/Exam/no_5.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() { return 0; }
+void nonstatic();
+void staticfn();
+
+int main() {
+  for (int i = 1; i <= 3; i++) {
+    nonstatic();
+    staticfn();
+  }
+  return 0;
+}
+
+void staticfn() {
+  static int value = 3;
+  cout << "static " << value - 2 << ": " << value << '\n';
+  value++;
+}
 
 void nonstatic() {
-  int value = 0;
-  cout << "value: " << value << '\n';
+  int value = 3;
+  cout << "nonstatic: " << value << '\n';
   value++;
 }
