c441ba77cc98f7f2cdc8e3f06215c4a6b1b24859
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-11 20:23:41 +0630

    no_6 start of each word capitalized

--- submitted/Exam/no_6.cpp
+++ submitted/Exam/no_6.cpp
@@ -15,6 +15,28 @@ int main() {
 
   cout << "Original string" << '\n';
   cout << str << '\n';
+  cout << '\n';
+
+  for (i = 0; i < n; i++) {
+    int temp = reverse_str[i];
+    reverse_str[i] = reverse_str[n - 1];
+    reverse_str[n - 1] = temp;
+    n--;
+  }
+
+  cout << "Reversed string" << '\n';
+  cout << reverse_str << '\n';
+  cout << '\n';
+
+  cout << "Start of each word capitalized" << '\n';
+  for (i = 0; i < capitalized_str[i] != '\0'; i++) {
+    if (i == 0)
+      capitalized_str[i] = toupper(capitalized_str[i]);
+    else if (isspace(capitalized_str[i - 1]))
+      capitalized_str[i] = toupper(capitalized_str[i]);
+  }
+  cout << capitalized_str << '\n';
+  cout << '\n';
 
   return 0;
 }
