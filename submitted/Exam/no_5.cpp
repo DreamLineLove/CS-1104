3b8386f7acb157204e7e8d10c79be226c5599876
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-11 19:13:47 +0630

    no_5 own version added

--- submitted/Exam/no_5.cpp
+++ submitted/Exam/no_5.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void nonstatic();
-void staticfn();
+float running_avg(const float input);
 
 int main() {
-  for (int i = 1; i <= 3; i++) {
-    nonstatic();
-    staticfn();
-  }
+  float v;
+
+  do {
+    cout << "\nEnter number: ";
+    cin >> v;
+    cout << "Running average is: " << running_avg(v) << '\n';
+  } while (v > 0);
+
   return 0;
 }
 
-void staticfn() {
-  static int value = 3;
-  cout << "static " << value - 2 << ": " << value << '\n';
-  value++;
-}
+float running_avg(const float input) {
+  static float sum = 0, count = 0;
+  count++;
 
-void nonstatic() {
-  int value = 3;
-  cout << "nonstatic: " << value << '\n';
-  value++;
+  sum = sum + input;
+  cout << "Current" << sum << '\n';
+  return sum / count;
 }
