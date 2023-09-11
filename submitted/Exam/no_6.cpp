53b17990a044c10145c230a47902387592214152
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-11 20:06:54 +0630

    No_6 started

--- submitted/Exam/no_6.cpp
+++ submitted/Exam/no_6.cpp
@@ -1,20 +1,19 @@
+#include <cctype>
 #include <cstring>
 #include <iostream>
 using namespace std;
 
 int main() {
   char str[] = "This is a cat";
+  char reverse_str[80];
+  char capitalized_str[80];
 
-  int i, n = strlen(str);
+  strcpy(reverse_str, str);
+  strcpy(capitalized_str, str);
 
-  for (i = 0; i < n; i++) {
-    int temp = str[i];
-    str[i] = str[n - 1];
-    str[n - 1] = temp;
-    n--;
-    cout << n << '\n';
-  }
+  int i, n = strlen(reverse_str);
 
+  cout << "Original string" << '\n';
   cout << str << '\n';
 
   return 0;
