bb431816113de8647286f0a52a733880a207bf1f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-16 02:33:04 +0630

    no_5 easier version added

--- finals/no_5.cpp
+++ finals/no_5.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
 
-float running_avg(float i);
-
-int main() {
-  float num = 0;
-
-  do {
-    cout << "Enter number (-1 to quit): ";
-    cin >> num;
-    if (num != -1)
-      cout << "Running average is: " << running_avg(num) << '\n';
-    cout << '\n';
-  } while (num > -1);
-
-  return 0;
-}
-
 float running_avg(float i) {
   static float sum = 0;
   static int count = 0;
@@ -26,4 +10,20 @@ float running_avg(float i) {
   count++;
 
   return sum / count;
+}
+
+int main() {
+  float num = 0;
+
+  do {
+    cout << "Enter number (-1 to quit): ";
+    cin >> num;
+
+    if (num > -1)
+      cout << "Running average is: " << running_avg(num) << '\n';
+    cout << '\n';
+
+  } while (num > -1);
+
+  return 0;
 }
