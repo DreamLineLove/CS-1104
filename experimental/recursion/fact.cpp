50b281f2bfa52b92ec25b7bcb2d5e984cf0bdada
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-16 00:12:52 +0630

    factorial with iteration

--- experimental/recursion/fact.cpp
+++ experimental/recursion/fact.cpp
@@ -8,8 +8,17 @@ int fact(int n) {
   return n * fact(n - 1);
 }
 
+int facti(int n) {
+  int ans = 1;
+  for (int i = 1; i <= n; i++) {
+    ans *= i;
+  }
+  return ans;
+}
+
 int main() {
   cout << fact(5) << '\n';
+  cout << facti(5) << '\n';
 
   return 0;
 }
