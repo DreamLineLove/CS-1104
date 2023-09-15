6220952cd7b41b91db0d4175d6a3e325d3056d2f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-16 01:46:11 +0630

    running average written

---
+++ experimental/static/no_5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int running_avg(float num) {
+  static float sum = 0;
+  static float count = 0;
+
+  sum += num;
+  count++;
+
+  return sum / count;
+}
+
+int main() {
+  float num;
+
+  do {
+    cout << "pick a number: ";
+    cin >> num;
+
+    if (num != -1)
+      cout << "running average is " << running_avg(num) << '\n';
+
+  } while (num > -1);
+
+  return 0;
+}
