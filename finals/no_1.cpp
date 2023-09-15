0ac11cbf99f72469b354dcbd506c323c3af24724
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-16 02:34:08 +0630

    no_1 new version where function is declared before main

--- finals/no_1.cpp
+++ finals/no_1.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int facti(int n);
-int factr(int n);
-
-int main() {
-  cout << "9 factorial is " << facti(9) << " (iterative)\n";
-  cout << "9 factorial is " << factr(9) << " (recursive)\n";
-
-  return 0;
-}
-
 int facti(int n) {
   int result = 1;
 
@@ -26,4 +16,11 @@ int factr(int n) {
     return 1;
 
   return n * factr(n - 1);
+}
+
+int main() {
+  cout << "9 factorial is " << facti(9) << " (iterative)\n";
+  cout << "9 factorial is " << factr(9) << " (recursive)\n";
+
+  return 0;
 }
