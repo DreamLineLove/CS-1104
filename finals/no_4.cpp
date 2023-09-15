28ee5cec14706a90448a4393ae3a41b23347e28f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-16 02:28:16 +0630

    no_4 easier version adjusted

--- finals/no_4.cpp
+++ finals/no_4.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void selection_sort(int arr[], int n);
+void selection_sort(int arr[], int n) {
+  int i, j;
+
+  for (i = 0; i < n - 1; i++) {
+    int min = i;
+
+    for (j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min])
+        min = j;
+    }
+
+    if (min != i)
+      swap(arr[min], arr[i]);
+  }
+}
 
 int main() {
   int marks[6] = {520, 491, 503, 476, 454, 439};
@@ -24,20 +38,4 @@ int main() {
   cout << "minimum mark: " << marks[0] << '\n';
 
   return 0;
-}
-
-void selection_sort(int arr[], int n) {
-  int i, j;
-
-  for (i = 0; i < n - 1; i++) {
-    int min = i;
-
-    for (j = i + 1; j < n; j++) {
-      if (arr[j] < arr[min])
-        min = j;
-    }
-
-    if (min != i)
-      swap(arr[min], arr[i]);
-  }
 }
