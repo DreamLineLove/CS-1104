7d7ee7bf21cbb7c7dc798b2295dd5de455af6bdd
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-16 00:06:40 +0630

    sum with recursion

--- experimental/recursion/sum.cpp
+++ experimental/recursion/sum.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() { return 0; }
+int sum(int n) {
+  if (n <= 0) {
+    return n;
+  }
+
+  return n + sum(n - 1);
+}
+
+int main() {
+  cout << sum(5) << '\n';
+  // cout << sumi(5) << '\n';
+
+  return 0;
+}
